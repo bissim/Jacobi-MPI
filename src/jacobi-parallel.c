85387c2bdaebd085a64437de342a40ee08da1aab
Simone B <s.bisogno10@studenti.unisa.it>
2020-03-27 06:53:14 +0100

    Update Jacobi MPI Release 0.1.0-beta+20200327

--- src/jacobi-parallel.c
+++ src/jacobi-parallel.c
@@ -3,7 +3,7 @@
  * @ingroup runnable
  * @author Simone Bisogno (bissim.github.io)
  * @brief Parallel version of Jacobi method.
- * @version 0.1.0-beta+20200316
+ * @version 0.1.0-beta+20200327
  * @date 2020-02-04
  *
  * @copyright Copyright (c) 2020
