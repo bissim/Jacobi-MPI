1e2a33952a5fab2e124f1738fe2d71cf7c03c584
Simone B <s.bisogno10@studenti.unisa.it>
2020-04-02 00:45:19 +0200

    Update Jacobi MPI to 0.1.0-beta+20200402 a4659ba: Update run-jacobi.sh

--- src/jacobi-serial.c
+++ src/jacobi-serial.c
@@ -4,7 +4,7 @@
  * @ingroup runnable
  * @author Simone Bisogno (bissim.github.io)
  * @brief Serial version of Jacobi method.
- * @version 0.1.0-beta+20200401.2330
+ * @version 0.1.0-beta+20200402
  * @date 2020-02-03
  *
  * @copyright Copyright (c) 2020
