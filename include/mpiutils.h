642748fe05639494bcb05d1bb6f8d7bf1f0a2331
Simone Bisogno <s.bisogno10@studenti.unisa.it>
2020-04-03 19:17:27 +0200

    Update Jacobi MPI to 0.1.0-beta+20200403.1915

--- include/mpiutils.h
+++ include/mpiutils.h
@@ -3,7 +3,7 @@
  * @ingroup headers
  * @author Simone Bisogno (bissim.github.io)
  * @brief Header file for MPI utility functions.
- * @version 0.1.0-beta+20200403
+ * @version 0.1.0-beta+20200403.1915
  * @date 2020-02-07
  * 
  * @copyright Copyright (c) 2020
--- src/jacobi-parallel.c
+++ src/jacobi-parallel.c
@@ -3,7 +3,7 @@
  * @ingroup runnable
  * @author Simone Bisogno (bissim.github.io)
  * @brief Parallel version of Jacobi method.
- * @version 0.1.0-beta+20200403
+ * @version 0.1.0-beta+20200403.1915
  * @date 2020-02-04
  *
  * @copyright Copyright (c) 2020
